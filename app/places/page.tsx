e380c43b55d6286767d5b203abdb3b8f53f840cd
naol17 <it.naol.dame12@gmail.com>
2023-06-09 23:05:33 +0300

    add pics

--- app/places/page.tsx
+++ app/places/page.tsx
@@ -1,3 +1,28 @@
+type PlaceImage = {
+  image: string
+}
+
+const placeImages: PlaceImage[] = [
+  {
+    image: "https://unsplash.com/photos/zUNs99PGDg0",
+  },
+  {
+    image: "https://unsplash.com/photos/6VhPY27jdps",
+  },
+  {
+    image: "https://unsplash.com/photos/zUNs99PGDg0",
+  },
+  {
+    image: "https://unsplash.com/photos/6VhPY27jdps",
+  },
+  {
+    image: "https://unsplash.com/photos/zUNs99PGDg0",
+  },
+  {
+    image: "https://unsplash.com/photos/6VhPY27jdps",
+  },
+]
+
 const Places = () => {
   return (
     <div className="flex items-center justify-center w-full h-full">
@@ -6,28 +31,15 @@ const Places = () => {
       {/* place image slider  */}
       <section className="bg-gray-100">
         <div className="container flex gap-6 py-6 overflow-x-auto overflow-y-hidden hide-scroll-bar max-w-max">
-          {nearbyPlaces.map((place, i) => (
+          {placeImages.map((place, i) => (
             <Card key={i} className="shadow-lg group">
-              <Link href="/" className="group-hover:animate-pulse">
+              <Link href="/" className="group-hover:animate-zoomin">
                 <CardContent className="relative p-0">
                   <img
                     src={place.image}
                     className="rounded-md !max-w-[500px]"
                     alt={place.name}
                   />
-                  <div className="absolute flex flex-col gap-2 text-white bottom-5 left-4">
-                    <h3 className="w-full text-2xl font-semibold ">
-                      {place.name}
-                    </h3>
-                    <p>
-                      {place.description.length > 50
-                        ? place.description.slice(0, 50) + "..."
-                        : place.description}
-                    </p>
-                  </div>
-                  <Badge className="absolute px-4 text-lg top-3 left-5">
-                    Add
-                  </Badge>
                 </CardContent>
               </Link>
             </Card>
