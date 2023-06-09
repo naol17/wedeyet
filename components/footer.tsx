3fe12a807dd643457c2564e19948b7c9fd842d5e
naol17 <it.naol.dame12@gmail.com>
2023-06-09 22:57:13 +0300

    pace image slider

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,20 +5,20 @@ import { Facebook, Instagram, Mail, MapPin, Phone, Twitter } from "lucide-react"
 const Footer = () => {
   return (
     <footer>
-      <div className="container py-16 space-y-8 lg:space-y-16">
+      <div className="container space-y-8 py-16 lg:space-y-16">
         <div className="grid grid-cols-1 gap-8 lg:grid-cols-4">
           <div className="lg:col-span-2">
             <div className="flex items-center text-primary">
               <Image src="/logo.png" height={50} width={50} alt="logo" />
             </div>
 
-            <p className="max-w-xs mt-4 text-gray-500">
+            <p className="mt-4 max-w-xs text-gray-500">
               Simplifying Addis Ababa&apos;s navigation with ease. Discover,
               explore, and navigate the city hassle-free. Your go-to app for
               finding shops, cafes, hospitals, and more.
             </p>
 
-            <ul className="flex gap-6 mt-8">
+            <ul className="mt-8 flex gap-6">
               <li>
                 <Link
                   href="/"
@@ -27,7 +27,7 @@ const Footer = () => {
                   className="text-gray-500 transition hover:opacity-75"
                 >
                   <span className="sr-only">Facebook</span>
-                  <Facebook className="w-6 h-6" />
+                  <Facebook className="h-6 w-6" />
                 </Link>
               </li>
 
@@ -39,7 +39,7 @@ const Footer = () => {
                   className="text-gray-500 transition hover:opacity-75"
                 >
                   <span className="sr-only">Instagram</span>
-                  <Instagram className="w-6 h-6" />
+                  <Instagram className="h-6 w-6" />
                 </Link>
               </li>
 
@@ -52,7 +52,7 @@ const Footer = () => {
                 >
                   <span className="sr-only">Twitter</span>
 
-                  <Twitter className="w-6 h-6" />
+                  <Twitter className="h-6 w-6" />
                 </Link>
               </li>
             </ul>
@@ -116,7 +116,7 @@ const Footer = () => {
                     className="flex items-center justify-center gap-1.5 ltr:sm:justify-start rtl:sm:justify-end"
                     href="/"
                   >
-                    <Mail className="w-5 h-5 shrink-0" />
+                    <Mail className="h-5 w-5 shrink-0" />
 
                     <span className="flex-1 text-gray-500">
                       info@wedeyet.com
@@ -129,14 +129,14 @@ const Footer = () => {
                     className="flex items-center justify-center gap-1.5 ltr:sm:justify-start rtl:sm:justify-end"
                     href="/"
                   >
-                    <Phone className="w-5 h-5 shrink-0" />
+                    <Phone className="h-5 w-5 shrink-0" />
 
                     <span className="flex-1 text-gray-500">+251 917846893</span>
                   </Link>
                 </li>
 
                 <li className="flex items-start justify-center gap-1.5 ltr:sm:justify-start rtl:sm:justify-end">
-                  <MapPin className="w-5 h-5 shrink-0" />
+                  <MapPin className="h-5 w-5 shrink-0" />
                   <address className="-mt-0.5 flex-1 not-italic text-gray-500">
                     Addis Ababa, Ethiopia
                   </address>
