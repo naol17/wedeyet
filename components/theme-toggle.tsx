70dc435ebea1a7a187c9ad784bc1c74a3067aca4
naol17 <it.naol.dame12@gmail.com>
2023-07-27 16:49:12 +0300

    toggler

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -10,15 +10,16 @@ export function ThemeToggle() {
   const { setTheme, theme } = useTheme()
 
   return (
-    <Button
-      variant="ghost"
-      size="sm"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      className="mt-8 sm:mt-16 lg:mt-5"
-    >
-      <Icons.sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Icons.moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      <span className="sr-only">Toggle theme</span>
-    </Button>
+    // <Button
+    //   variant="ghost"
+    //   size="sm"
+    //   // onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+    //   className="mt-8 sm:mt-16 lg:mt-5"
+    // >
+    //   <Icons.sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+    //   <Icons.moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    //   <span className="sr-only">Toggle theme</span>
+    // </Button>
+    <div></div>
   )
 }
