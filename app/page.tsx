a28e3c1838e9a6312c1d789fa56c899ce0a83f9a
naol17 <it.naol.dame12@gmail.com>
2023-07-27 16:21:25 +0300

    hd

--- app/page.tsx
+++ app/page.tsx
@@ -122,7 +122,7 @@ export default function IndexPage() {
   const fetchData = () => {
     const headers: any = {
       Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjQ0YTFiMTU3ODUyODRlYmEwNjYyOTY5IiwiZW1haWwiOiJudW5hQGdtYWlsLmNvbSIsImlhdCI6MTY5MDM3NjA3OSwiZXhwIjoxNjkwNDYyNDc5fQ.VEpeM-TENpUikjcxxzSbGXFE3dlXjSMZmQhltCZZ0ZA",
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjQ0YTFiMTU3ODUyODRlYmEwNjYyOTY5IiwiZW1haWwiOiJudW5hQGdtYWlsLmNvbSIsImlhdCI6MTY5MDQzNDkyNCwiZXhwIjoxNjkwNTIxMzI0fQ.y9pdUTF3QMtg0ZnCP_tEYgElyNI9MkldasslI_gEVY4",
     }
     axios
       .get("https://wedeyet.herokuapp.com/api/place/all", { headers })
