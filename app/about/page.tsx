5067738c5881d34d55e1e2bf402282046f45d6b7
naol17 <it.naol.dame12@gmail.com>
2023-08-30 20:48:49 +0300

    hjk

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,48 @@
 const About = () => {
   return (
     <>
-      <div className="flex items-center justify-center w-full h-full">
-        <h1 className="text-5xl">Coming soon</h1>
+      <div className="flex items-center justify-center w-full h-full mt-5">
+        <h1 className="text-4xl">Wel-come</h1>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 justify-center items-center  mt-8">
+        <div className="lg:col-span-1 md:col-span-1 w-full p-6">
+          <img src="./hello.svg" alt="Contact Us" className="w-full h-auto" />
+        </div>
+        <div className="lg:col-span-1 md:col-span-1 w-full p-6">
+          <h1 className="justify-center text-2xl">About us</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt
+            dolorem suscipit deserunt quis voluptatibus, culpa ullam architecto
+            impedit hic provident fuga cumque voluptatem, quo repellendus ab
+            laborum, libero possimus ea?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt
+            dolorem suscipit deserunt quis voluptatibus, culpa ullam architecto
+            impedit hic provident fuga cumque voluptatem, quo repellendus ab
+            laborum, libero possimus ea?
+          </p>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 justify-center items-center  mt-8">
+        <div className="lg:col-span-1 md:col-span-1 w-full p-6">
+          <h1 className="justify-center text-2xl">About us</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt
+            dolorem suscipit deserunt quis voluptatibus, culpa ullam architecto
+            impedit hic provident fuga cumque voluptatem, quo repellendus ab
+            laborum, libero possimus ea?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt
+            dolorem suscipit deserunt quis voluptatibus, culpa ullam architecto
+            impedit hic provident fuga cumque voluptatem, quo repellendus ab
+            laborum, libero possimus ea?
+          </p>
+        </div>
+        <div className="lg:col-span-1 md:col-span-1 w-full p-6">
+          <img src="./hello.svg" alt="Contact Us" className="w-full h-auto" />
+        </div>
       </div>
     </>
   )
