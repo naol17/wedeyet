65679139adda1dd158488e4bdc9520bd39e08202
naol17 <it.naol.dame12@gmail.com>
2023-08-31 17:46:07 +0300

    nav update

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -16,68 +16,62 @@ interface MainNavProps {
 export function MainNav({ items }: MainNavProps) {
   const [isOpen, setIsOpen] = useState(false)
   return (
-    <nav className="flex flex-wrap items-center justify-between lg:pr-10 lg:pt-0  sm:mr-6  sm:mt-7 lg:bg-inherit lg:mr-40 ">
-      <div className="block lg:hidden">
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-          // asi lawxin jiru
-          className="mr-14 mt-1 flex items-center rounded px-3 py-2 pt-10 text-primary  "
-        >
-          <svg
-            className={`fill-current h-5 w-5 pt-[-20rem] ${
-              isOpen ? "hidden" : "block"
-            }`}
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
+    // <nav className="flex flex-wrap items-center justify-between lg:pr-10 lg:pt-0  sm:mr-6  sm:mt-7 lg:bg-inherit lg:mr-40 ">
+    //   <div className="block lg:hidden">
+    //     <button
+    //       onClick={() => setIsOpen(!isOpen)}
+    //       // asi lawxin jiru
+    //       className="mr-14 mt-1 flex items-center rounded px-3 py-2 pt-10 text-primary  "
+    //     >
+    //       <svg
+    //         className={`fill-current h-5 w-5 pt-[-20rem] ${
+    //           isOpen ? "hidden" : "block"
+    //         }`}
+    //         viewBox="0 0 20 20"
+    //         xmlns="http://www.w3.org/2000/svg"
+    //       >
+    //         <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+    //       </svg>
 
-          <svg
-            className={`fill-current h-5 w-5 ${isOpen ? "block" : "hidden"}`}
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
-          </svg>
-        </button>
-      </div>
+    //       <svg
+    //         className={`fill-current h-5 w-5 ${isOpen ? "block" : "hidden"}`}
+    //         viewBox="0 0 20 20"
+    //         xmlns="http://www.w3.org/2000/svg"
+    //       >
+    //         <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+    //       </svg>
+    //     </button>
+    //   </div>
+    //   <div
+    //     className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto xs:bg-slate-500  ${
+    //       isOpen ? "block" : "hidden"
+    //     }`}
+    //   >
+    //     <div className="ml-10 mt-[-26rem] bg-green-300  pl-3  pt-[22rem] text-sm  lg:mt-0 lg:flex lg:bg-inherit lg:pt-0  dark:text-white ">
+    //       <a
+    //         href="/"
+    //         className="block mt-1 pt-7 lg:pt-0 lg:inline-block lg:mt-0 text-white-200 mr-4 border-white dark:hover:bg-slate-800 "
+    //       >
+    //         Home
+    //       </a>
 
-      <div
-        className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto xs:bg-slate-500  ${
-          isOpen ? "block" : "hidden"
-        }`}
-      >
-        <div className="ml-10 mt-[-26rem] bg-green-300  pl-3  pt-[22rem] text-sm  lg:mt-0 lg:flex lg:bg-inherit lg:pt-0  dark:text-white ">
-          <a
-            href="/"
-            className="block mt-1 pt-7 lg:pt-0 lg:inline-block lg:mt-0 text-white-200 mr-4 border-white dark:hover:bg-slate-800 "
-          >
-            Home
-          </a>
+    //       <a
+    //         href="about"
+    //         className="text-white-200 mr-4 mt-1 block lg:mt-0 lg:inline-block dark:hover:bg-slate-800"
+    //       >
+    //         About
+    //       </a>
 
-          {/* <a
-            href="/pages"
-            className="mt-1lg:inline-block text-white-200 mr-4 block lg:mt-0 dark:hover:bg-slate-800"
-          >
-            place
-          </a> */}
+    //       <a
+    //         href="contact"
+    //         className="block mt-1 lg:inline-block lg:mt-0 text-white-200 mr-4 dark:hover:bg-slate-800"
+    //       >
+    //         Contact
+    //       </a>
+    //     </div>
+    //   </div>
+    // </nav>
 
-          <a
-            href="about"
-            className="text-white-200 mr-4 mt-1 block lg:mt-0 lg:inline-block dark:hover:bg-slate-800"
-          >
-            About
-          </a>
-
-          <a
-            href="contact"
-            className="block mt-1 lg:inline-block lg:mt-0 text-white-200 mr-4 dark:hover:bg-slate-800"
-          >
-            Contact
-          </a>
-        </div>
-      </div>
-    </nav>
+    <div></div>
   )
 }
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -43,9 +43,15 @@ export function SiteHeader() {
     setFilteredPlaces(filteredResults)
   }, [searchQuery])
   console.log("searching", searchQuery)
+
+  const [showMenu, setShowMenu] = useState(false)
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu)
+  }
   return (
     <>
-      <header className="sticky top-0 z-40 w-full border-b bg-background flex items-center justify-between flex-wrap  pb-5 sm:pb-10 lg:pb-5">
+      {/* <header className="sticky top-0 z-40 w-full border-b bg-background flex items-center justify-between flex-wrap  pb-5 sm:pb-10 lg:pb-5">
         <div className="container flex items-center h-16 space-x-4 sm:justify-between sm:space-x-0 lg:ml-36 lg:mr-14">
           <div className="flex items-center flex-shrink-0 text-white mr-6 lg:mr-60 sm:mr-40 mt-8 sm:mt-12 lg:mt-5">
             <img src="/logo.png" className="w-100 h-10 mr-2" alt="Logo" />
@@ -72,7 +78,206 @@ export function SiteHeader() {
             </nav>
           </div>
         </div>
-      </header>
+      </header> */}
+
+      {/* **********************************************
+////////////////////////////////////////////////// */}
+
+      {/* <nav className="bg-gray-800">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex-shrink-0">
+              <a href="#" className="text-white font-bold text-xl">
+                Logo
+              </a>
+            </div>
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Home
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  About
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Services
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Contact
+                </a>
+              </div>
+            </div>
+            <div className="md:hidden">
+              <button
+                type="button"
+                className="text-gray-300 hover:text-white focus:outline-none focus:text-white"
+                onClick={toggleMenu}
+              >
+                <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                  {showMenu ? (
+                    <path
+                      className="hidden"
+                      fillRule="evenodd"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  ) : (
+                    <path
+                      className="block"
+                      fillRule="evenodd"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  )}
+                </svg>
+              </button>
+            </div>
+          </div>
+          {showMenu && (
+            <div className="md:hidden">
+              <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Home
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  About
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Services
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Contact
+                </a>
+              </div>
+            </div>
+          )}
+        </div>
+      </nav> */}
+      <nav className="bg-gray-800">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex-shrink-0 flex items-center">
+              <a href="#" className="text-white font-bold text-xl">
+                Logo
+              </a>
+            </div>
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Home
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  About
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Services
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Contact
+                </a>
+              </div>
+            </div>
+            <div className="ml-4">
+              <input
+                type="text"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                placeholder="Search for a place"
+                className="bg-inherit border-primary rounded-md px-4 py-2 focus:outline-none"
+              />
+            </div>
+            <div className="md:hidden">
+              <button
+                type="button"
+                className="dark:text-white hover:text-gray-400 focus:outline-none focus:text-white"
+                onClick={toggleMenu}
+              >
+                <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                  {showMenu ? (
+                    <path
+                      className="hidden"
+                      fillRule="evenodd"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  ) : (
+                    <path
+                      className="block"
+                      fillRule="evenodd"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  )}
+                </svg>
+              </button>
+            </div>
+          </div>
+          {showMenu && (
+            <div className="md:hidden">
+              <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Home
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  About
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Services
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Contact
+                </a>
+              </div>
+            </div>
+          )}
+        </div>
+      </nav>
+      {/* ********************************************
+       ************************************************ */}
       {searchQuery !== "" ? (
         <div className=" ml-8 md:mr-10 lg:mr-10 mr-4">
           {filteredPlaces.map((place) => (
