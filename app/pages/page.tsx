9933baa1ad04c82f88cd43dcd17a5592139e79ab
naol17 <it.naol.dame12@gmail.com>
2023-10-19 16:47:25 +0300

    uy

--- app/pages/page.tsx
+++ app/pages/page.tsx
@@ -93,7 +93,7 @@ const page = () => {
     console.log("inside fetch", id)
     const headers: any = {
       Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjUwZTg4OWI0NmIxNjExNmYyN2Y2MzMyIiwiZW1haWwiOiJhZG1pbkBudW5hLmNvbSIsImlhdCI6MTY5Njc5NzcwMywiZXhwIjoxNjk3MjI5NzAzfQ.khcTLOGPejYdO92VIbFaWJ1pbsazObz7BZBYuWZCxhY",
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjUwZTg4OWI0NmIxNjExNmYyN2Y2MzMyIiwiZW1haWwiOiJhZG1pbkBudW5hLmNvbSIsImlhdCI6MTY5NzcxNzQxOCwiZXhwIjoxNjk4MTQ5NDE4fQ.3iOtLZ0A1B55my3elCl-_ZxmS773wYuaEI3oEHrf0Qo",
     }
     axios
       .get(`https://wedeyet.herokuapp.com/api/place/get/${id}`, { headers })
@@ -118,7 +118,7 @@ const page = () => {
   const AdsData = () => {
     const headers: any = {
       Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjUwZTg4OWI0NmIxNjExNmYyN2Y2MzMyIiwiZW1haWwiOiJhZG1pbkBudW5hLmNvbSIsImlhdCI6MTY5Njc5NzcwMywiZXhwIjoxNjk3MjI5NzAzfQ.khcTLOGPejYdO92VIbFaWJ1pbsazObz7BZBYuWZCxhY",
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjUwZTg4OWI0NmIxNjExNmYyN2Y2MzMyIiwiZW1haWwiOiJhZG1pbkBudW5hLmNvbSIsImlhdCI6MTY5NzcxNzQxOCwiZXhwIjoxNjk4MTQ5NDE4fQ.3iOtLZ0A1B55my3elCl-_ZxmS773wYuaEI3oEHrf0Qo",
     }
     axios
       .get("https://wedeyet.herokuapp.com/api/banner/all", { headers })
@@ -146,7 +146,7 @@ const page = () => {
     console.log("inside subCategory", subCategory)
     const headers: any = {
       Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjUwZTg4OWI0NmIxNjExNmYyN2Y2MzMyIiwiZW1haWwiOiJhZG1pbkBudW5hLmNvbSIsImlhdCI6MTY5Njc5NzcwMywiZXhwIjoxNjk3MjI5NzAzfQ.khcTLOGPejYdO92VIbFaWJ1pbsazObz7BZBYuWZCxhY",
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjUwZTg4OWI0NmIxNjExNmYyN2Y2MzMyIiwiZW1haWwiOiJhZG1pbkBudW5hLmNvbSIsImlhdCI6MTY5NzcxNzQxOCwiZXhwIjoxNjk4MTQ5NDE4fQ.3iOtLZ0A1B55my3elCl-_ZxmS773wYuaEI3oEHrf0Qo",
     }
     axios
       .get(
